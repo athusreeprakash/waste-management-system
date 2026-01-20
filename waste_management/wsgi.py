441a52ae43625c4bd65458b9858ac72a536df469
Athusrèèè <140880758+athusreeprakash@users.noreply.github.com>
2026-01-20 12:33:15 +0530

    Add WSGI configuration for waste_management project

---
+++ waste_management/wsgi.py
@@ -0,0 +1,13 @@
+"""
+WSGI config for waste_management project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'waste_management.settings')
+
+application = get_wsgi_application()
